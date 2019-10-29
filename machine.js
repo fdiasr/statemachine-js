7f42b099760bead7f695af61d646f222d810bf3d
Fernando Dias <fdias.webdev@gmail.com>
2019-10-29 01:51:38 -0200

    Add dispatcher and change status to sm

--- machine.js
+++ machine.js
@@ -11,5 +11,14 @@ const machine = {
     'error': {
       retry: function () { }
     }
+  },
+  dispatch(action, ...payload) {
+    const action = this.transitions[this.state][actionName]
+    if (action) {
+      action.apply(machine, ...payload)
+    }
+  },
+  changeStatusTo(newState) {
+    this.state = newState
   }
 }
